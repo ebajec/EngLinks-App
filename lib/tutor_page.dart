b6e7a11e0120e971ba1f18c3a8c1bcc86607fb9f
Eli <20ebd1@queensu.ca>
2023-03-14 17:51:12 -0400

    More work on tutor page.  Fixed json formatting

--- lib/tutor_page.dart
+++ lib/tutor_page.dart
@@ -1,6 +1,8 @@
 import 'package:englinks_app/my_styles.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 import 'app_state.dart';
 
 /* Tutoring page will need to have input boxes for the user and a button to send
@@ -44,17 +46,66 @@ class TutorForm extends StatefulWidget {
 class TutorFormState extends State<TutorForm> {
   final _formKey = GlobalKey<FormState>();
 
-  String? _name;
+  Map<String, String> formData = {
+    'name': '',
+    'course': '',
+    'freq': '',
+    'comment': ''
+  };
+
   String? _email;
-  String? _course;
-  String? _tutorFrequency;
-  String? _comment;
 
   List<String> _courses = ['APSC 111', 'APSC 112'];
   List<String> _tutorTimes = ['Weekly', 'Bi-weekly'];
 
   String? _selectedCourse;
   String? _selectedTutorTime;
+
+  Future<String> postTutorRequest(
+      Uri url, Object? body, BuildContext context) async {
+    ScaffoldMessenger.of(context)
+        .showSnackBar(SnackBar(content: Text('Sending request...')));
+
+    http.Response? response;
+
+    try {
+      var response = await http.post(url, body: body);
+      return response.body;
+    } catch (e) {
+      return 'Unable to contact server';
+    }
+  }
+
+  bool validateName(String name) {
+    int commas = 0;
+    int validChars = 0;
+
+    int i = 0;
+    do {
+      String c = name[i];
+
+      //check character
+      if (c == ',') {
+        if (validChars == 0) {
+          return false;
+        }
+
+        commas++;
+        validChars = 0;
+      } else if (c != ' ') {
+        validChars++;
+      }
+
+      //fail conditions
+      if (commas > 1) {
+        return false;
+      }
+
+      i++;
+    } while (i < name.length);
+
+    return (validChars > 0) && (commas == 1);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -88,17 +139,20 @@ class TutorFormState extends State<TutorForm> {
                   TextFormField(
                     onChanged: (value) {
                       setState(() {
-                        _name = value;
+                        formData['name'] = value;
                       });
                     },
                     validator: (value) {
                       if (value == null || value.isEmpty) {
                         return 'This field is required.';
                       }
+                      if (!validateName(value)) {
+                        return 'Invalid format!';
+                      }
                       return null;
                     },
                     decoration: InputDecoration(
-                      labelText: 'First, Last',
+                      labelText: 'Last, First',
                       border: OutlineInputBorder(),
                     ),
                   ),
@@ -136,7 +190,7 @@ class TutorFormState extends State<TutorForm> {
                   FormDropdownInput<String?>(
                     onSaved: (value) {
                       setState(() {
-                        _course = value;
+                        if (value != null) formData['course'] = value;
                       });
                     },
                     value: _selectedCourse,
@@ -158,7 +212,7 @@ class TutorFormState extends State<TutorForm> {
                   ),
                   FormDropdownInput<String?>(
                     onSaved: (value) {
-                      _tutorFrequency = value;
+                      if (value != null) formData['freq'] = value;
                     },
                     value: _selectedTutorTime,
                     items: _tutorTimes,
@@ -179,11 +233,14 @@ class TutorFormState extends State<TutorForm> {
                   ),
                   TextFormField(
                     validator: (value) {
-                      _comment ??= '';
+                      String? comment = formData['comment'];
+                      if (comment != '' && comment!.length > 1024) {
+                        return 'Character limit is 1024';
+                      }
                     },
                     onChanged: (value) {
                       setState(() {
-                        _comment = value;
+                        formData['comment'] = value;
                       });
                     },
                     keyboardType: TextInputType.multiline,
@@ -199,18 +256,18 @@ class TutorFormState extends State<TutorForm> {
           Padding(
             padding: const EdgeInsets.all(18),
             child: ElevatedButton(
-              onPressed: () {
+              onPressed: () async {
                 if (_formKey.currentState!.validate()) {
-                  String formData = '''{
-                    '"name":"${_name!}",
-                    "course":"${_course!}",
-                    "freq":"${_tutorFrequency!}",
-                    "comment":"${_comment!}" 
-                  }''';
+                  String formDataJSON = json.encode(formData);
 
-                  ScaffoldMessenger.of(context).showSnackBar(
-                    SnackBar(content: Text(formData)),
-                  );
+                  var url =
+                      Uri.http('192.168.2.92:5000', 'recieve_tutor_request');
+
+                  var response =
+                      await postTutorRequest(url, formDataJSON, context);
+
+                  ScaffoldMessenger.of(context)
+                      .showSnackBar(SnackBar(content: Text(response)));
                 }
               },
               style: ElevatedButton.styleFrom(
