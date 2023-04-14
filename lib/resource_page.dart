624462f8d262a389d3d5e586e82046b38d673958
Eli Bajec-Dickinson <tacoman164@gmail.com>
2023-04-14 13:29:25 -0400

    changed titles to match rest of app

--- lib/resource_page.dart
+++ lib/resource_page.dart
@@ -1,12 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:url_launcher/url_launcher.dart';
 import 'app_state.dart';
 import 'my_styles.dart';
 import 'package:flutter_linkify/flutter_linkify.dart';
-
-
-
-
+import 'my_widgets.dart';
 
 /*Learning resources stuff.  Should mostly be links. */
 class ResourcePage extends StatelessWidget {
@@ -16,75 +14,55 @@ class ResourcePage extends StatelessWidget {
   }
 }
 
-class ResourceData extends StatelessWidget {
+Uri resourcesFirstYear =
+    Uri.http("englinks.ca/resources/online-resources/first-year-resources/");
 
+class ResourceData extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.all(8.0),
       child: Column(
         children: [
-          Text(
+          FeatureTitle(
             'Textbook Resources',
-            style: TextStyle(
-                fontSize: 40,
-                color: Colors.purple,
-                fontWeight: FontWeight.w900),
+            textSize: 22,
           ),
           Container(
             padding: EdgeInsets.all(20),
             child: Text(
                 'The EngLinks goal is to offer quality educational resources to engineering students with no charge. Any learning resources that could be seen as useful to engineering students will happily be taken by EngLinks and published.  Furthermore, any old textbooks that could be donated by the university would be greatly appreciated. '),
           ),
+          FeatureTitle(
+            'Hours of Operation',
+            textSize: 22,
+          ),
           Container(
             padding: EdgeInsets.all(20),
             child: Text(
-              'Hours of Operation',
-              style: TextStyle(
-                  fontSize: 40,
-                  color: Colors.purple,
-                  fontWeight: FontWeight.w900),
-            ),
+                'Monday to Thursday - 5:00 PM to 11:00 PM\n Saturday - 11:30 AM to 7:00 PM\n Sunday - 11:30 AM to 11:00 PM'),
           ),
-          Container( 
-            padding : EdgeInsets.all(20),
-            child: Text(
-              'Monday to Thursday - 5:00 PM to 11:00 PM\n Saturday - 11:30 AM to 7:00 PM\n Sunday - 11:30 AM to 11:00 PM'
-            ),
+          FeatureTitle('Resources', textSize: 22),
+          InkWell(
+            child: Text("First year resources"),
+            onTap: () => launchUrl(resourcesFirstYear),
           ),
-         Container(
-          padding: EdgeInsets.all(20),
-          child: Text(
-            'Resources',
-            style: TextStyle(
-              fontSize: 40,
-              color: Colors.purple,
-              fontWeight: FontWeight.w900),
-          ),
-         ),
-        Linkify(
-          onOpen: (link) => print("Clicked ${link.url}!"),
-          text: "First year resources : https://englinks.ca/resources/online-resources/first-year-resources/\n Upper Year Resources : https://englinks.ca/resources/online-resources/upper-year-resources/",
-        ),
-        Container(
-          padding: EdgeInsets.all(20),
-          child: Text(
+          //Linkify(
+          //  onOpen: (link) => print("Clicked ${link.url}!"),
+
+          //  text:
+          //      "First year resources : https://englinks.ca/resources/online-resources/first-year-resources/\n Upper Year Resources : https://englinks.ca/resources/online-resources/upper-year-resources/",
+          //),
+          FeatureTitle(
             'Contacts',
-            style: TextStyle(
-              fontSize: 40,
-              color: Colors.purple,
-              fontWeight: FontWeight.w900),
+            textSize: 22,
           ),
-        ),
-        
-
-        SelectableLinkify(
-          text: "First Year Contact : englinks.firstyear@engsoc.queensu.ca\nUpper Year Contact : englinks.upperyear@engsoc.queensu.ca",
-        ),
-
+          SelectableLinkify(
+            text:
+                "First Year Contact : englinks.firstyear@engsoc.queensu.ca\nUpper Year Contact : englinks.upperyear@engsoc.queensu.ca",
+          ),
         ],
       ),
     );
   }
 }
-
